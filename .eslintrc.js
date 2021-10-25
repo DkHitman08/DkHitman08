952eed3e7548923731700f84d5236e631fa998b7
DkHitman08 <93066448+DkHitman08@users.noreply.github.com>
2021-10-25 21:00:20 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+  1 =  {
+  2 parser:  '@typescript-eslint/parser', 
+  3  extends:  [
+  4    'plugin:@typescript-eslint/recommended',  
+  5 ],
+  6 parserOptions:  {
+  7  ecmaVersion:  2020,  
+  8    sourceType:  'module',  
+  9  },
+  10 rules:  {
+
+   11 },
+  12 }
